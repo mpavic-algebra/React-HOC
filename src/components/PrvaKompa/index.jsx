e499b8a941027e4642c6756ea21e78d009e62952
Matej Pavic <matej@Matejs-MacBook-Pro.local>
2023-09-26 19:12:57 +0200

    Add inline styles

--- src/components/PrvaKompa/index.jsx
+++ src/components/PrvaKompa/index.jsx
@@ -2,10 +2,19 @@ import withCount from "../../withCount"
 import styles from "./PrvaKompa.module.css"
 
 const PrvaKompa = ({count, increment}) => {
+  const countStyles = {
+    backgroundColor: "blue",
+    opacity: .5
+  }
+
+  const btnStyles = {
+    // backgroundColor: "blue",
+    color: "blue"
+  }
   return (
     <>
-        <div>{count}</div>
-        <button className={styles.btn} onClick={increment}>Increment</button>
+        <div style={countStyles}>{count}</div>
+        <button style={btnStyles} className={styles.btn} onClick={increment}>Increment</button>
     </>
   )
 }
